b15f153affac6be5b082e1e96cedc003c3158b62
AleksBarahona <al02891318@tecmilenio.mx>
2024-04-24 18:36:55 -0600

    Conexion a supabase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://ooohhtymjrxmqcpwefrw.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vb2hodHltanJ4bXFjcHdlZnJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMTUwODUsImV4cCI6MjAyODg5MTA4NX0.B8bGJ69-NRpyL1RrcS_1B2lK8sLNUjO_8IJk4n7Jjh8");
 
 function App() {
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+
+  async function getUsuarios() {
+    const { data } = await supabase.from("usuarios").select();
+    setUsuarios(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {usuarios.map((usuario) => (
+        <li key={usuario.nombre}>{usuario.nombre}</li>
+      ))}
+    </ul>
   );
 }
 
